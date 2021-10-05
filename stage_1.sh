65d12859805fa1061211061d582b5c83e3197b05
shridhar <shridharmishra4@gmail.com>
2021-10-05 14:15:36 -0700

    remove venv, soft rest

--- stage_1.sh
+++ stage_1.sh
@@ -1,3 +1,3 @@
-/Users/shridhar/miniconda3/envs/label/bin/python annotate_corners_v1.1.py $1
-/Users/shridhar/miniconda3/envs/label/bin/python bbox_annotation_using_points.py $1
-/Users/shridhar/miniconda3/envs/label/bin/python heatmap_v3.py $1
+./cabinet/bin/python annotate_corners_v1.1.py $1
+./cabinet/bin/python bbox_annotation_using_points.py $1
+./cabinet/bin/python heatmap_v3.py $1
